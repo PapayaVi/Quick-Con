e7d36204680302bd1d4c4cb4103b0b9e1d38206b
reymart-vi <110107868+reymart-vi@users.noreply.github.com>
2024-09-18 00:46:50 +0800

    init

---
+++ server/api/index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express()
+
+require('dotenv').config()
+
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const dbConfig = require('../db.config');
+const mysql = require('mysql2/promise');
+
+const db = mysql.createPool({
+        ...dbConfig,
+        namedPlaceholders: true,
+      });
+
+// Create a table to store chat messages if it doesn't exist
+db.execute(`
+        CREATE TABLE IF NOT EXISTS chat_messages (
+          id INT AUTO_INCREMENT,
+          message TEXT,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (id)
+        );
+      `).then(() => {
+        console.log('Chat messages table created');
+      }).catch((err) => {
+        console.error('Error creating chat messages table:', err);
+      });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.set("view engine", "ejs",);
+app.set("views", path.join(__dirname,'/../views'));
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {  
+        res.render("home");
+});
+
+io.on('connection', (socket) => {
+        // Load previous messages from the database
+        db.execute("SELECT message,DATE_FORMAT(CONVERT_TZ(created_at, '+00:00', '+08:00'), '%h:%i %p') AS time FROM chat_messages ORDER BY created_at ASC").then(([results, fields]) => {
+                results.forEach((res_obj) => {
+                        io.emit('chat message', {message : res_obj.message, time : res_obj.time} );
+                });
+        }).catch((err) => {
+                console.error('Error loading previous messages:', err);
+        });
+
+        socket.on('disconnect', () => {
+                io.emit('chat message', 'user has left...');;
+        });
+
+        socket.on('chat message', (msg) => {
+                // Save the message to the database
+                db.execute('INSERT INTO chat_messages (message) VALUES (:message)', { message: msg })
+                .then((result) => {
+                const insertedId = result[0].insertId;
+               
+                db.execute("SELECT message,DATE_FORMAT(CONVERT_TZ(created_at, '+00:00', '+08:00'), '%h:%i %p') AS time FROM chat_messages WHERE id = ?", [insertedId])
+                .then((rows) => {
+                        const insertedData = rows[0];
+                        io.emit('chat message', insertedData[0]);
+                })
+                .catch((err) => {
+                        console.error('Error fetching inserted data:', err);
+                });
+                })
+                .catch((err) => {
+                console.error('Error saving message:', err);
+                });
+        });
+});
+
+server.listen(3000, () => {
+        console.log('Server is now live at --- \u001b[1;32m http://localhost:'+port);
+});
---
+++ server/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+        host: process.env.AIVEN_MYSQL_HOST,
+        port: process.env.AIVEN_MYSQL_PORT,
+        user: process.env.AIVEN_MYSQL_USERNAME,
+        password: process.env.AIVEN_MYSQL_PASSWORD,
+        database: process.env.AIVEN_MYSQL_DB
+};
